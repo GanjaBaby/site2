78b37f2a9b12d379ba124eecf5320bd261240e12
Parth Sinha <41264578+sinhaparth5@users.noreply.github.com>
2024-04-22 11:13:29 +0100

    Delete frontend directory

--- frontend/assets/scripts/home.js
+++
@@ -1,7 +0,0 @@
-
-
-function submitStory() {
-    var storyContent = document.getElementById("story").value;
-    console.log(storyContent);
-    // You can add further processing or sending data to a server here
-}
